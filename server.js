d6ba85597d1ae34dbcc7650f7d96c1140c79af1b
Alex Kipling <alex.kipling@developmentnow.com>
2015-01-28 11:56:44 -0800

    BUTTON DOESNT FUCKING WORK

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	jade = require('jade'),
+	stylus = require('stylus'),
+	nib = require('nib'),
+	port = process.env.PORT || 3000,
+	app = express();
+
+function compile(str, path) {
+	return stylus(str)
+		.set('filename', path)
+		.use(nib())
+}
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(stylus.middleware(
+  { src: __dirname + '/public',
+  compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(req, res) {
+	var backEndArray = ["C", "C++", "Golang", "Python", "Ruby on Rails", "Node.js", "Express", "Flatiron", "Taco", "Sails", "Java", "PHP", "Perl", "Erlang", "D", "Xhp", "ASP.NET", "Scala", "Smalltalk", "COBOL on Wheelchair", "FORTRAN"]
+	var frontEndArray = ["AngularJS", "ERB", "HAML", "Jade", "Backbone.js", "Thorax", "Batman", "Meteor", "React", "Flight", "Ember.js", "Knockout", "Singool.js", "Maria", "Rivets.js", "Synapse", "Objective-C", "Swift"]
+	var databaseArray = ["PostgreSQL", "MySQL", "MS Access", "Oracle Express", "SQLite", "MSSQL", "NoSQL"]
+	var cssThemesArray = ["Bootstrap", "Jumpstart", "Darkstrap", "jQuery Mobile Bootstrap", "SOME KIND OF WORDPRESS SHIT", "CSS3 Microsoft Modern Buttons", "BootMetro", "Cosmo", "Phonegap"]
+
+	var stack = {
+		backEnd: backEndArray[Math.floor(Math.random() * backEndArray.length)].toUpperCase(),
+		frontEnd: frontEndArray[Math.floor(Math.random() * frontEndArray.length)].toUpperCase(),
+		database: databaseArray[Math.floor(Math.random() * databaseArray.length)].toUpperCase(),
+		cssThemes: cssThemesArray[Math.floor(Math.random() * cssThemesArray.length)].toUpperCase()
+	}
+
+	res.render('index', stack)
+});
+
+app.listen(port);
+console.log("Server loaded. The magic happens on port", port)
